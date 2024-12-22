34f247ba146e6e779f6b24b1e6570989f36ae3c9
downpour07 <choiheeyoung10@gmail.com>
2024-12-22 20:01:03 +0900

    feat :: login navigation setting

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import UserScreen from './src/navigation/UserScreen';
+import Loading from './src/screens/Loading';
 
-export default function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <UserScreen />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/Loading/index.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import { View, TouchableWithoutFeedback, Keyboard, StyleSheet } from "react-native";
+import { color } from '../../styles/Theme';
+import Constants from '../../styles/Constants';
+
+const Loading = () => {
+
+    return(
+        <View></View>
+    );
+}
+
+const Styles = StyleSheet.create({
+
+})
+
+export default Loading;
