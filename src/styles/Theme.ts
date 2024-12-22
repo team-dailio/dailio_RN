d5cdb5b1d023a3690b77559bc459d31063f7eb59
downpour07 <choiheeyoung10@gmail.com>
2024-12-22 18:20:12 +0900

    feat :: color system 설정

---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+export const color = {
+    White: "#FFFFFF",
+    Black: "#1F1E21",
+    Info: "#FAF450",
+    Success: "#44E633",
+    Error: "#E63338",
+    Gray: ["#FAFAFA", "#EEEEEE", "#9E9E9E", "#616161", "#212121"],
+    Primary: ["#EAE6FF", "#9985FF", "#5133E6", "#2B0FB8", "#1A038E"],
+};
